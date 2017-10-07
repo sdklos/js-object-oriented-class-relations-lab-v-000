429db486422e0700e6426696806a5dbd77df80f0
sukey <sdklos@gmail.com>
2017-10-07 18:51:41 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,80 @@
+let store = {drivers: [], passengers: [], trips: []}
+
+let driverId = 0
+let passengerId = 0
+let tripId = 0
+
+class Driver {
+
+  constructor(name) {
+    this.id = ++driverId
+    this.name = name
+
+    store.drivers.push(this)
+  }
+
+  trips() {
+    return store.trips.filter(function(trip) {
+      return driverId === this.id
+    }.bind(this));
+  };
+
+  passengers() {
+    let trips = this.trips()
+    return trips.map(function(trip) {
+      return trip.passenger()
+    })
+  }
+
+}
+
+class Passenger {
+
+  constructor(name) {
+    this.id = ++passengerId
+    this.name = name
+
+    store.passengers.push(this)
+  }
+
+  trips() {
+    return store.trips.filter(function(trip) {
+      return trip.passenger() === this
+    }.bind(this));
+  };
+
+  drivers() {
+    return this.trips().map(function(trip) {
+      return trip.driver()
+    })
+  }
+}
+
+class Trip {
+
+  constructor(driver, passenger) {
+    this.id = ++tripId
+    if(driver){
+      this.driverId = driver.id
+    }
+    if(passenger){
+      this.passengerId = passenger.id
+    }
+
+    store.trips.push(this)
+  }
+
+  passenger(){
+    return store.passengers.find(function(passenger)
+    {
+      return passenger.id === this.passengerId
+    }.bind(this));
+  };
+
+  driver(){
+    return store.drivers.find(function(driver){
+      return driver.id === this.driverId
+    }.bind(this));
+  };
+
+}
